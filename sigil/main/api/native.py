ccadb7df89bd3dca2366e663cb9ab3e87118057b
marshallvoid <hieptt.dev@gmail.com>
2025-08-20 15:31:49 +0700

    chore: add vercel.json

--- sigil/main/api/native.py
+++ sigil/main/api/native.py
@@ -6,3 +6,7 @@ settings = get_settings()
 container = make_container(settings=settings)
 factory = APIFactory(container=container, settings=settings)
 app = factory.make()
+
+
+if __name__ == "__main__":
+    factory.run(app=app, host="0.0.0.0", port=8000)
